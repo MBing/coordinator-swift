a3144532d879875336f44201d986b730f2e8651d
Martin Bing <bing.martin.photography@gmail.com>
2019-03-27 10:15:46 +0100

    Simple coordinator pattern

---
+++ Coordinators/MainCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  MainCoordinator.swift
+//  Coordinators
+//
+//  Created by Martin Demiddel on 27.03.19.
+//  Copyright © 2019 Martin Bing. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        self.navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func buySubscription() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+}

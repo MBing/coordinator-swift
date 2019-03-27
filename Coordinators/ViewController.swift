d685ccd3d272e5aabed4d44044bf9418e8c0177c
Martin Bing <bing.martin.photography@gmail.com>
2019-03-27 08:28:32 +0100

    Initial Commit

---
+++ Coordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinators
+//
+//  Created by Martin Demiddel on 27.03.19.
+//  Copyright © 2019 Martin Bing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
